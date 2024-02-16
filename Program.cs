5db07c4e76d4aba990e94c1e3f67c45983189e8c
rajabov0011 <rajabov_1999@list.ru>
2024-02-16 16:01:40 +0500

    First Upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿string firstinput = Console.ReadLine();
+string secondinput = Console.ReadLine();
+
+int firstnumber = Convert.ToInt32(firstinput);
+int secondnumber = Convert.ToInt32(secondinput);
+
+Console.WriteLine(firstnumber + secondnumber);
